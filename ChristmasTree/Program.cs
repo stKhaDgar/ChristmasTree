68c2ed816bb9134f1996a6e0b8f1d2969e55055d
stKhaDgar <33667544+stKhaDgar@users.noreply.github.com>
2017-11-30 17:21:03 +0200

    All is ready (without snow)

--- ChristmasTree/Program.cs
+++ ChristmasTree/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 
 namespace SoloLearn
@@ -15,18 +16,27 @@ namespace SoloLearn
             tree.Create();
         }
     }
-
+    
+    // Class of the Tree
     public class ChristmasTree
     {
         public char[,] Snow = new char[12, 40];
+        
         public ChristmasTree()
         {
             for(int j=0; j < 40; j++)
             {
                 Snow[0, j] = ' ';
             }
+            for (int j = 10; j < 12; j++)
+            {
+                for(int i=0; i<40; i++)
+                {
+                    Snow[j, i] = ' ';
+                }
+            }
 
-            for(int j=1; j<10; j++)
+            for (int j=1; j<10; j++)
             {
                 for(int i=15; i>=j; i--)
                 { 
@@ -40,13 +50,15 @@ namespace SoloLearn
             }
             for (int j = 10; j < 12; j++)
             {
-                for(int i=15; i<=17;i++)
+                for(int i=14; i<=16;i++)
                 {
                     Snow[j, i] = '|';
                     i++;
                 }
             }
         }
+
+        // Print tree and snow on monitor
         public void Create()
         {
             for (int i = 0; i < 12; i++)
@@ -58,7 +70,6 @@ namespace SoloLearn
                 Console.Write('\n');
             }
         }
+        
     }
-
-
 }
